2b456c1be218a5d30fbb8eb9609031de3fd10600
Ricardo Delgado <Ricardo.Delgado@endava.com>
2023-07-07 11:36:07 -0500

    Implements reactive app

--- src/main/java/com/hello/Application.java
+++ src/main/java/com/hello/Application.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringReactiveRedisApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringReactiveRedisApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
-
 }
--- src/test/java/com/hello/ApplicationTests.java
+++ src/test/java/com/hello/ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringReactiveRedisApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
